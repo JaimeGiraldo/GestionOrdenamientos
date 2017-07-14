4b9a3e71e6c750a400401f728503bcb7ab3269d2
JaimeGiraldo <jgiraldog@gmail.com>
2017-07-14 12:16:58 -0500

    prueba

---
+++ GestionOrdenamientos/ImportarArchivo.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionOrdenamientos
+{
+    /// <summary>
+    /// Descripción breve de ImportarArchivo
+    /// </summary>
+    public class ImportarArchivo : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "texto/normal";
+            context.Response.Write("Hola a todos");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
